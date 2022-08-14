6cfaa5fda649ebbbe989a85a8c67710097b50e2a
Christian Behrens <chrbehrens@users.noreply.github.com>
2022-08-14 16:57:39 +0200

    last change

--- main.c
+++ main.c
@@ -7,5 +7,6 @@ int main(int argc, char *argv[])
     printf("a new feature add2Integer(2,3): %d\n", add2Integer(2,3));
     printf("   ...one more feature\n");
     printf("   ...und ein letztes feature");    
+    printf("   ...nun aber wirklich das letztes feature");  
     return 0;
 }
