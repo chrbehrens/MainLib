a3d6785dcd536ef8e9b9fb209f56ce306a62c462
Christian Behrens <chrbehrens@users.noreply.github.com>
2022-08-14 16:32:32 +0200

    one more feature

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main(int argc, char *argv[])
 {
     printf("this is in the master branch!\n");
     printf("a new feature add2Integer(2,3): %d\n", add2Integer(2,3));
+    printf("   ...one more feature\n")
     return 0;
 }
